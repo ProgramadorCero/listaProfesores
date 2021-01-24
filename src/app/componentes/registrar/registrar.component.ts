517df01f360cf1573157a67e8a9a0ec5c34b4782
Oliver <terrazaoliver@americana.edu.co>
2021-01-24 06:49:30 -0500

    notificacion de datos invalido en el formulario registro por alertify

--- src/app/componentes/registrar/registrar.component.ts
+++ src/app/componentes/registrar/registrar.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ValidacionFormularioService } from 'src/app/compartidos/validacion-formulario/validacion-formulario.service';
 import { AuthServiceService } from '../../compartidos/auth/auth-service.service';
+import { Profesor } from '../../compartidos/interfaces/profesor';
 declare let alertify: any;
 
 
@@ -15,6 +16,7 @@ export class RegistrarComponent implements OnInit {
 
   formularioRegistro!: FormGroup;
   validacionFormularioRegistro = ValidacionFormularioService.validacionRegistro;
+  profesor!:Profesor;
   constructor(
     private auth: AuthServiceService,
     public  modalReferencia: MatDialogRef <RegistrarComponent>,
@@ -50,9 +52,74 @@ export class RegistrarComponent implements OnInit {
 
   obtenerDatosFormularioRegistro( elemento: FormGroup): void
   {
-    alertify.set('notifier', 'position', 'top-center');
-    alertify.success('Profesor registrado exitosamente');
-    console.log(elemento);
+    if (this.formularioValido())
+    {
+      this.inicializarObjetoProfesor(elemento.value);
+    }
+    else
+    {
+      for (let propiedad in this.formularioRegistro.getRawValue())
+      {
+        console.log(this.campoFormularioRegistroTieneError(propiedad))
+        if (this.campoFormularioRegistroTieneError(propiedad))
+        {
+          this.mensajeDeErrorSegunElCampoRegistro(propiedad);
+        }
+      }
+    }
+  }
+
+  mensajeDeErrorSegunElCampoRegistro(propiedad:string): void
+  {
+    switch (propiedad)
+    {
+      case 'nombre':
+        this.mensajeErrorAlertify('ERROR EN EL CAMPO NOMBRE');
+      break
+      case 'email':
+        this.mensajeErrorAlertify('ERROR EN EL CAMPO CORREO ELECTRONICO');
+      break
+      case 'identificacion':
+        this.mensajeErrorAlertify('ERROR EN EL CAMPO CORREO IDENTIFICACIÓN');
+      break
+      case 'password':
+        this.mensajeErrorAlertify('ERROR EN EL CAMPO CORREO CONTRASEÑA');
+      break
+      default:
+        console.log('no hay errores');
+      break
+    }
+  }
+
+  mensajeErrorAlertify(mensaje: string):void
+  {
+    alertify.set('notifier','position', 'top-right');
+    alertify.error(mensaje);
+  }
+
+  formularioValido(): boolean
+  {
+    return this.formularioRegistro.valid;
+  }
+
+  inicializarObjetoProfesor(elemento: any): void
+  {
+    this.profesor = {
+      nombre: elemento.nombre,
+      identificacion: elemento.identificacion,
+      password: elemento.password,
+      email: elemento.email
+    }
+    console.log(this.profesor);
+  }
+
+  campoFormularioRegistroTieneError(nombreCampo: string): boolean
+  {
+    if( this.formularioRegistro.get(nombreCampo)?.invalid)
+    {
+      return true;
+    }
+    return false;
   }
 
 }
